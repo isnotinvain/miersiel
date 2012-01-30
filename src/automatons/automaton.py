d7f48420048db81d2258a98ced81d61bee53a183
Alex Levenson <alex@isnotinvain.com>
2012-01-29 17:37:23 -0800

    Adding in state transition functionality

--- src/automatons/automaton.py
+++ src/automatons/automaton.py
@@ -1,12 +1,21 @@
-import math
+from langutil import Enum
 
 class Automaton(object):
+	ONE_STATE = Enum.new("default")
+	DEFAULT_STATE = ONE_STATE.default
 
-	def __init__(self, env, simulation, sensors=None):
+	def __init__(self, env, simulation, sensors=None, states=ONE_STATE, defaultState=DEFAULT_STATE, onStateTransition=None):
 		self.simulation = simulation
 		self.env = env
-		if not sensors:
+		self.sensors = sensors
+		if not self.sensors:
 			self.sensors = {}
+		if not issubclass(states, Enum):
+			states = Enum.new(*states)
+		self.states = states
+		self.defaultState = defaultState
+		self.currentState = defaultState
+		self.onStateTransition = onStateTransition
 
 	def getCenter():
 		pass
@@ -22,3 +31,10 @@ class Automaton(object):
 
 	def kill(self):
 		pass
+
+	def changeState(self, state):
+		if not hasattr(self.states, str(state)):
+			raise ValueError("Invalid state change, I have no knowlege of state: " + str(state))
+		if self.onStateTransition:
+			self.onStateTransition(self.currentState, state)
+		self.currentState = state
---
+++ src/langutil.py
@@ -0,0 +1,7 @@
+class Enum(object):
+	@classmethod
+	def new(cls, *seq, **named):
+		if len(seq) != len(set(seq)):
+			raise ValueError("Duplicate keys in enum:" + str(seq))
+		enums = dict(zip(seq, seq), **named)
+		return type('Enum', (cls,), enums)
